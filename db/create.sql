9fe03bfe2f80868ceca055bcd181cf5f88828614
github username <github email address>
2018-10-01 11:13:30 -0400

    Done.

--- db/create.sql
+++ db/create.sql
@@ -1,1 +1,9 @@
 CREATE TABLE dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT);
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS dogs (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+  )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
